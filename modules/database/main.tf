ff658d25ddc17eab7ea84bbce258fd950d2d75ef
aminabhi50 <aminabhi50@gmail.com>
2024-11-05 18:02:07 +0000

    Updated

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -1,21 +1,26 @@
-resource "azurerm_mysql_server" "mysql" {
-  name                = "ecommerce-mysql"
+resource "azurerm_mysql_flexible_server" "mysql" {
+  name                = "ecommerce-mysql-server"
   location            = var.location
   resource_group_name = var.resource_group_name
-  administrator_login = var.admin_login
-  administrator_login_password = var.admin_password
+  admin_username      = var.admin_login
+  admin_password      = var.admin_password
   sku_name            = var.sku_name
-  storage_profile {
+
+  storage {
     storage_mb            = var.storage_mb
     backup_retention_days = var.backup_retention_days
     geo_redundant_backup  = var.geo_redundant_backup
   }
+
+  high_availability {
+    mode = "ZoneRedundant" # Or "SameZone" depending on your requirement
+  }
 }
 
-resource "azurerm_mysql_database" "ecommerce_db" {
+resource "azurerm_mysql_flexible_database" "ecommerce_db" {
   name                = "ecommerce_db"
   resource_group_name = var.resource_group_name
-  server_name         = azurerm_mysql_server.mysql.name
-  charset             = "utf8"
+  server_name         = azurerm_mysql_flexible_server.mysql.name
   collation           = "utf8_general_ci"
+  charset             = "utf8"
 }
