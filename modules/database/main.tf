54148583eaa7330a3e3e41a414dd0daea679ebe1
aminabhi50 <aminabhi50@gmail.com>
2024-11-06 08:04:30 +0000

    Updated

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_mssql_server" "sql_server" {
   name                         = var.sql_server_name
-  resource_group_name          = azurerm_resource_group.ecommerce_rg.name
+  resource_group_name          = var.resource_group_name
   location                     = var.location
   version                      = "12.0"
   administrator_login          = var.sql_admin_username
